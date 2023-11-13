8c89d2a4e1017500d6ad6d3d8b6c9f5f9e8d825f
thelaw51 <joegillard3@gmail.com>
2023-11-13 21:48:39 +0000

    Add jwilsson/spotify-web-api-php package and related files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8a5d861650202b61575b0be2a1d182ae27af6533',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8a5d861650202b61575b0be2a1d182ae27af6533',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jwilsson/spotify-web-api-php' => array(
+            'pretty_version' => '4.3.0',
+            'version' => '4.3.0.0',
+            'reference' => 'adcea818d7a850dc42202def000f818c0207b512',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jwilsson/spotify-web-api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
